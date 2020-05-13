b9ecdf541c537e611fa858131177fd572ccd6d54
aDissamal <nusrachel@hotmail.com>
2020-05-13 19:15:00 -0400

    RM: create Client, Server and ClientHandler

--- INF3405 - TP1/src/Client.java
+++ INF3405 - TP1/src/Client.java
@@ -1,4 +1,24 @@
+import java.io.DataInputStream;
+import java.net.Socket;
 
 public class Client {
-
+	
+	private static Socket socket;
+	
+	public static void main(String[] args) throws Exception {
+		String serverAddress = "127.0.0.1";
+		int port = 5000;
+		
+		socket = new Socket(serverAddress, port);
+		
+		System.out.format("Client - The server is running on %s:%d%n", serverAddress, port);
+		
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+		
+		String helloMesssageFromServer = in.readUTF();
+		
+		System.out.println("Client - " + helloMesssageFromServer);
+		
+		socket.close();	
+    }    
 }
--- INF3405 - TP1/src/Server.java
+++ INF3405 - TP1/src/Server.java
@@ -1,4 +1,48 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-
+	private static ServerSocket listener;
+	
+	/*
+	 *	Application Serveur 
+	 */
+	public static void main(String[] args) throws Exception {
+		// Compteur incrémenté à chaque connexion d'un client au serveur
+		int clientNumber = 0;
+		
+		// Adress et port du serveur
+		String serverAddress = "127.0.0.1";
+		int serverPort = 5000;
+		
+		// Création de la connexion pour communiquer avec les clients
+		listener = new ServerSocket();
+		listener.setReuseAddress(true);
+		InetAddress serverIP = InetAddress.getByName(serverAddress);
+		
+		// Association de l'adresse et du port à la connexion
+		listener.bind(new InetSocketAddress(serverIP, serverPort));
+		
+		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
+		
+		try {
+			/*
+			 * À chaque fois qu'un nouveau client se connecte, on exécute la fonction
+			 * Run() de l'objet ClientHandler
+			 */
+			while (true) {
+				// Important : la fonction accept() est bloquante : attend qu'un prochain client se connecte
+				// Une nouvelle connection : on incrémente le compter clientNumber
+				new ClientHandler(listener.accept(), clientNumber++).start();
+			}
+		}
+		finally {
+			// Fermeture de la connexion
+			listener.close();
+		}		
+	}
 }
