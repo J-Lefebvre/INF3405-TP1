5f74dcdc6cd12718c80ee0a26d10f64632cdc029
aDissamal <nusrachel@hotmail.com>
2020-05-13 19:23:53 -0400

    RM: moved ClientHandler class to Server.java

--- INF3405 - TP1/src/Client.java
+++ INF3405 - TP1/src/Client.java
@@ -5,20 +5,28 @@ public class Client {
 	
 	private static Socket socket;
 	
+	/*
+	 *  Application client
+	 */
+	
 	public static void main(String[] args) throws Exception {
+		// Adresse et port du serveur
 		String serverAddress = "127.0.0.1";
 		int port = 5000;
 		
+		// Création d'une nouvelle connexion avec le serveur
 		socket = new Socket(serverAddress, port);
 		
 		System.out.format("Client - The server is running on %s:%d%n", serverAddress, port);
 		
+		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur
 		DataInputStream in = new DataInputStream(socket.getInputStream());
 		
-		String helloMesssageFromServer = in.readUTF();
-		
+		// Attente de la réception d'un message envoyé par le serveur sur le canal
+		String helloMesssageFromServer = in.readUTF();		
 		System.out.println("Client - " + helloMesssageFromServer);
 		
+		// Fermeture de la connexion avec le serveur
 		socket.close();	
     }    
 }
