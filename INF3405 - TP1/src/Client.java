ea50d64db501a539c005d165133e2c25cfe36fd2
Jules Lefebvre <jltlefebvre@gmail.com>
2020-05-31 10:44:25 -0400

    JL : validateAddress()

--- INF3405 - TP1/src/Client.java
+++ INF3405 - TP1/src/Client.java
@@ -4,47 +4,101 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-	
+
 	private static Socket socket;
-	
+
 	// Constantes
 	private static int ADDRESS_LENGTH = 4;
 	private static int PORT_RANGE_MIN = 5000;
-	private static int PORT_RANGE_MAX = 5050;	
-	
+	private static int PORT_RANGE_MAX = 5050;
+
+	// Création du scanner pour lire les entrées de l'utilisateur
+	private static Scanner sc = new Scanner(System.in);
+
+	// Adresse et port du serveur
+	private static String serverAddress;
+	private static int serverPort;
+
 	/*
 	 * Requis du client :
 	 * 
-	 * TODO : Saisie et validation des paramètres serveur (adresse IP, port)
-	 * TODO : Saisie et validation nom d'utilisateur et mot de passe
+	 * TODONE : Saisie et validation des paramètres serveur (adresse IP, port) 
+	 * TODO : Saisie et validation nom d'utilisateur et mot de passe 
 	 * TODO : Connexion au serveur
 	 * TODO : Réception des messages OU erreur mot de passe
 	 * TODO : Saisir une réponse (200 char maximum)
-	 * TODO : Transmettre la réponse au serveur
+	 * TODO : Transmettre la réponse au serveur 
 	 * TODO : Déconnexion
-	 * 
-	 * */
-	
+	 */
+
 	/*
-	 *  Application client
-	 */	
+	 * Application client
+	 */
 	public static void main(String[] args) throws Exception {
+
+		validateAddress();
 		
-		// Création du scanner pour lire les entrées de l'utilisateur
-		Scanner sc = new Scanner(System.in);
+		// Création d'une nouvelle connexion avec le serveur
+		socket = new Socket(serverAddress, serverPort);
 
-		// Adresse et port du serveur
-		String serverAddress;
-		int serverPort;
+		System.out.format("Client - The server is running on %s:%d%n", serverAddress, serverPort);
 
-		// Entrée et vérification de l'adresse IP	
-		boolean ipIsValid = false;		
+		System.out.println("Type \"quit()\" to close the server!");
+
+		// Création d'un canal sortant pour envoyer des messages au serveur
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+
+		// Création de la chaine contenant les messages à envoyer au server
+		String writeToServer;
+
+		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur
+		DataInputStream in = new DataInputStream(socket.getInputStream());
+
+		// Création de la chaine contenant les messages provenant du server
+		String readFromServer;
+
+		do {
+			// Entrée du nom d'utilisateur
+			System.out.print("Enter username: ");
+			writeToServer = sc.next();
+
+			// Envoi au serveur
+			out.writeUTF(writeToServer);
+
+			// validation ou création nouvel utilisateur dans la base de donnée
+			// entrée mot de passe
+			// validation ou message d'erreur
+		} while (!writeToServer.equals("quit()"));
+
+		// Fermeture du Scanner
+		sc.close();
+
+		/*
+		 * Extrait de code des notes de cours
+		 * 
+		 * 
+		 * // Création d'un canal entrant pour recevoir les messages envoyés par le
+		 * serveur DataInputStream in = new DataInputStream(socket.getInputStream());
+		 * 
+		 * // Attente de la réception d'un message envoyé par le serveur sur le canal
+		 * String helloMesssageFromServer = in.readUTF(); System.out.println("Client - "
+		 * + helloMesssageFromServer); *
+		 */
+
+		// Fermeture de la connexion avec le serveur
+		socket.close();
+
+	}
+
+	private static void validateAddress() {
+		// Entrée et vérification de l'adresse IP
+		boolean ipIsValid = false;
 		do {
 			// Conditions de validité
 			boolean hasFourBytes = false;
 			boolean bytesAreValid = true;
 
-			// Entrée de l'adresse IP du serveur		
+			// Entrée de l'adresse IP du serveur
 			System.out.print("Enter IP address: ");
 			serverAddress = sc.next();
 
@@ -60,21 +114,21 @@ public class Client {
 				for (int i = 0; i < ADDRESS_LENGTH; i++) {
 					ip[i] = Integer.parseInt(parts[i]);
 					bytesAreValid &= ip[i] >= 0 && ip[i] < 256;
-				}	
-			}		
+				}
+			}
 
 			// Vérification de l'adresse IP du serveur
 			ipIsValid = hasFourBytes && bytesAreValid;
 			if (!ipIsValid) {
 				System.err.println("Invalid IP address! Please try again...");
 			}
-		} while (!ipIsValid);	
+		} while (!ipIsValid);
 
 		// Entrée et vérification du port
 		boolean portIsValid = false;
 
 		do {
-			// Entrée du numéro de port		
+			// Entrée du numéro de port
 			System.out.print("Enter port: ");
 			serverPort = sc.nextInt();
 
@@ -83,62 +137,9 @@ public class Client {
 				portIsValid = true;
 			} else {
 				System.err.println("Invalid port! Please try again...");
-			}			
+			}
 		} while (!portIsValid);
-		
-		
-		// Création d'une nouvelle connexion avec le serveur 
-		socket = new Socket(serverAddress, serverPort);
-		
-		System.out.format("Client - The server is running on %s:%d%n", serverAddress,serverPort);
-		
-		
-		System.out.println("Type \"quit()\" to close the server!");
-		
-		// Création d'un canal sortant pour envoyer des messages au serveur
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		
-		// Création de la chaine contenant les messages à envoyer au server
-		String writeToServer;
-		
-		// Création d'un canal entrant pour recevoir les messages envoyés par le serveur 
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-			
-		// Création de la chaine contenant les messages provenant du server
-		String readFromServer;
-		
-		do {
-			// Entrée du nom d'utilisateur
-			System.out.print("Enter username: ");
-			writeToServer = sc.next();
-			
-			// Envoi au serveur
-			out.writeUTF(writeToServer);
-			
-			
-			// validation ou création nouvel utilisateur dans la base de donnée
-			// entrée mot de passe
-			// validation ou message d'erreur
-		} while (!writeToServer.equals("quit()"));
-		
-		// Fermeture du Scanner
-		sc.close();
-				
-		/* Extrait de code des notes de cours
-		 * 	
-		 * 
-		 * // Création d'un canal entrant pour recevoir les messages envoyés par le
-		 * serveur 
-		 * DataInputStream in = new DataInputStream(socket.getInputStream());
-		 * 
-		 * // Attente de la réception d'un message envoyé par le serveur sur le canal
-		 * String helloMesssageFromServer = in.readUTF(); System.out.println("Client - "
-		 * + helloMesssageFromServer);
-		 * 		 * 
-		 */
-		
-		// Fermeture de la connexion avec le serveur 
-		socket.close();
-			
-    }    
+
+	}
+
 }
--- INF3405 - TP1/src/Server.java
+++ INF3405 - TP1/src/Server.java
@@ -9,179 +9,184 @@ import java.util.Scanner;
 
 public class Server {
 	private static ServerSocket listener;
-	
+
 	// Constantes
 	private static int ADDRESS_LENGTH = 4;
 	private static int PORT_RANGE_MIN = 5000;
-	private static int PORT_RANGE_MAX = 5050;	
-	
+	private static int PORT_RANGE_MAX = 5050;
+
+	// Création du scanner pour lire les entrées de l'utilisateur
+	private static Scanner sc = new Scanner(System.in);
+
+	// Adresse et port du serveur
+	private static String serverAddress;
+	private static int serverPort;
+
 	/*
 	 * Requis du serveur:
 	 * 
-	 * TODONE: Saisie des paramètres du serveur (adresse IP, port d’écoute entre 5000 et 5050)
-	 * TODO: Pouvoir connecter des utilisateurs avec leurs mots de passe
-	 * TODO: Pouvoir envoyer un historique des sessions de clavardage
+	 * TODONE: Saisie des paramètres du serveur (adresse IP, port d’écoute entre
+	 * 5000 et 5050) 
+	 * TODO: Pouvoir connecter des utilisateurs avec leurs mots de passe 
+	 * TODO: Pouvoir envoyer un historique des sessions de clavardage 
 	 * TODO: Pouvoir fermer le serveur puis le rouvrir, et avoir tous les profils usagers et messages disponibles (écriture sur disque et pas uniquement sur RAM)
-	 * TODO: Recevoir les messages des clients
+	 * TODO: Recevoir les messages des clients 
 	 * TODO: Tenir un historique de toutes les messages.
 	 * TODO: Tenir une base de données des usagers et leurs mots de passe
-	 * TODO: Afficher en temps réel les messages
+	 * TODO: Afficher en temps réel les messages 
 	 * TODO: Effectuer correctement la vérification nom d’utilisateur/ mot de passe
 	 * TODO: Créer les comptes automatiquement s’ils n’existent pas
 	 */
-	
+
 	/*
-	 *	Application serveur 
+	 * Application serveur
 	 */
 	public static void main(String[] args) throws Exception {
+
 		// Compteur incrémenté à chaque connexion d'un client au serveur
 		int clientNumber = 0;
 		
-		// Création du scanner pour lire les entrées de l'utilisateur
-		Scanner sc = new Scanner(System.in);
-		
-		// Adresse et port du serveur
-		String serverAddress;
-		int serverPort;
-		
-		// Entrée et vérification de l'adresse IP	
-		boolean ipIsValid = false;		
-		do {
-			// Conditions de validité
-			boolean hasFourBytes = false;
-			boolean bytesAreValid = true;
-						
-			// Entrée de l'adresse IP du serveur		
-			System.out.print("Enter IP address: ");
-			serverAddress = sc.next();
-			
-			// Vérification de la longueur (nombre d'octets == 4)
-			int[] ip = new int[ADDRESS_LENGTH];
-			String[] parts = serverAddress.split("\\.");
-			if (parts.length == ADDRESS_LENGTH) {
-				hasFourBytes = true;
-			}
-			
-			// Vérification de la validité des octets (positifs et inférieurs à 256)
-			if (hasFourBytes) {
-				for (int i = 0; i < ADDRESS_LENGTH; i++) {
-					ip[i] = Integer.parseInt(parts[i]);
-					bytesAreValid &= ip[i] >= 0 && ip[i] < 256;
-				}	
-			}		
-			
-			// Vérification de l'adresse IP du serveur
-			ipIsValid = hasFourBytes && bytesAreValid;
-			if (!ipIsValid) {
-				System.err.println("Invalid IP address! Please try again...");
-			}
-		} while (!ipIsValid);	
-		
-		// Entrée et vérification du port
-		boolean portIsValid = false;
-		
-		do {
-			// Entrée de l'adresse IP du serveur		
-			System.out.print("Enter port: ");
-			serverPort = sc.nextInt();
-			
-			// Vérification du port du serveur
-			if (serverPort >= PORT_RANGE_MIN && serverPort <= PORT_RANGE_MAX) {
-				portIsValid = true;
-			} else {
-				System.err.println("Invalid port! Please try again...");
-			}			
-		} while (!portIsValid);
-		
-		// Fermeture du Scanner
-		sc.close();
-		
+		validateAddress();
+
 		// Création de la connexion pour communiquer avec les clients
 		listener = new ServerSocket();
 		listener.setReuseAddress(true);
 		InetAddress serverIP = InetAddress.getByName(serverAddress);
-		
+
 		// Association de l'adresse et du port à la connexion
 		listener.bind(new InetSocketAddress(serverIP, serverPort));
-		
+
 		System.out.format("The server is running on %s:%d%n", serverAddress, serverPort);
-		
+
 		try {
 			/*
-			 * À chaque fois qu'un nouveau client se connecte, on exécute la fonction run() de l'objet ClientHandler
+			 * À chaque fois qu'un nouveau client se connecte, on exécute la fonction run()
+			 * de l'objet ClientHandler
 			 */
 			while (true) {
-				// Important : la fonction accept() est bloquante : attend qu'un prochain client se connecte
+				// Important : la fonction accept() est bloquante : attend qu'un prochain client
+				// se connecte
 				// Une nouvelle connection : on incrémente le compter clientNumber
 				new ClientHandler(listener.accept(), clientNumber++).start();
 			}
-		}
-		finally {
+		} finally {
 			// Fermeture de la connexion
 			listener.close();
-		}		
+		}
 	}
-	
+
 	/*
-	 * Une thread qui se charge de traiter la demande de chaque client sur un socket particulier
+	 * Une thread qui se charge de traiter la demande de chaque client sur un socket
+	 * particulier
 	 */
 	private static class ClientHandler extends Thread {
 		private Socket socket;
 		private int clientNumber;
-		
-		public ClientHandler (Socket socket, int clientNumber) {
+
+		public ClientHandler(Socket socket, int clientNumber) {
 			this.socket = socket;
 			this.clientNumber = clientNumber;
-			System.out.println("New connection with client#" + clientNumber + " at " + socket);
+			System.out.println("New connection with client #" + clientNumber + " at " + socket.getLocalAddress() + ":"
+					+ socket.getLocalPort());
 		}
-		
+
 		/*
 		 * Une thread se charge d'envoyer au client un message de bienvenue
 		 */
 		public void run() {
 			try {
 				/*
-				// Envoi d'un message au client
-				out.writeUTF("Hello from server - you are client#" + clientNumber);
+				 * // Envoi d'un message au client
+				 * out.writeUTF("Hello from server - you are client#" + clientNumber);
 				 */
-				
+
 				// Création d'un canal entrant pour recevoir les messages du client
 				DataInputStream in = new DataInputStream(socket.getInputStream());
-				
+
 				// Création du lecteur des entrées du client
-				String readFromClient; 
-				
+				String readFromClient;
+
 				// Création d'un canal sortant pour envoyer des messages au client
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-				
+
 				// Création d'un canal sortant pour envoyer des messages au clients
 				String writeToClient;
-				
-				do {				
+
+				do {
 					// Lecture
 					readFromClient = in.readUTF();
-					
+
 					// Affichage
-					System.out.println(readFromClient);			
+					System.out.println(readFromClient);
 
-					
-
-					
-				} while (!readFromClient.equals("quit()"));			
+				} while (!readFromClient.equals("quit()"));
 			} catch (IOException e) {
-				System.out.println("Error handling client#" + clientNumber +": " + e);
-			}
-			finally {
+				System.out.println("Error handling client#" + clientNumber + ": " + e);
+			} finally {
 				try {
 					// Fermeture de la connexion avec le client
 					socket.close();
-				}
-				catch (IOException e) {
+				} catch (IOException e) {
 					System.out.println("Couldn't close a socket, what's going on?");
 				}
 				System.out.println("Connection with Client# " + clientNumber + " closed");
 			}
 		}
 	}
-}
 
+	private static void validateAddress() {
+
+		// Entrée et vérification de l'adresse IP
+		boolean ipIsValid = false;
+		do {
+			// Conditions de validité
+			boolean hasFourBytes = false;
+			boolean bytesAreValid = true;
+
+			// Entrée de l'adresse IP du serveur
+			System.out.print("Enter IP address: ");
+			serverAddress = sc.next();
+
+			// Vérification de la longueur (nombre d'octets == 4)
+			int[] ip = new int[ADDRESS_LENGTH];
+			String[] parts = serverAddress.split("\\.");
+			if (parts.length == ADDRESS_LENGTH) {
+				hasFourBytes = true;
+			}
+
+			// Vérification de la validité des octets (positifs et inférieurs à 256)
+			if (hasFourBytes) {
+				for (int i = 0; i < ADDRESS_LENGTH; i++) {
+					ip[i] = Integer.parseInt(parts[i]);
+					bytesAreValid &= ip[i] >= 0 && ip[i] < 256;
+				}
+			}
+
+			// Vérification de l'adresse IP du serveur
+			ipIsValid = hasFourBytes && bytesAreValid;
+			if (!ipIsValid) {
+				System.err.println("Invalid IP address! Please try again...");
+			}
+		} while (!ipIsValid);
+
+		// Entrée et vérification du port
+		boolean portIsValid = false;
+
+		do {
+			// Entrée de l'adresse IP du serveur
+			System.out.print("Enter port: ");
+			serverPort = sc.nextInt();
+
+			// Vérification du port du serveur
+			if (serverPort >= PORT_RANGE_MIN && serverPort <= PORT_RANGE_MAX) {
+				portIsValid = true;
+			} else {
+				System.err.println("Invalid port! Please try again...");
+			}
+		} while (!portIsValid);
+
+		// Fermeture du Scanner
+		sc.close();
+	}
+
+}
